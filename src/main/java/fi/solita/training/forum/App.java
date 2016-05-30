9189d5c9ce81d6449b8b4bbe808a2af4841a6721
Antti Huokko <antti.huokko@solita.fi>
2016-05-30 15:06:35 +0300

    Updated to be a Spring boot application

--- src/main/java/fi/solita/training/forum/App.java
+++ src/main/java/fi/solita/training/forum/App.java
@@ -1,11 +1,17 @@
 package fi.solita.training.forum;
 
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println(StringUtils.capitalize("hello world!!!"));
+		SpringApplication.run(App.class, args);
 	}
 
 }
