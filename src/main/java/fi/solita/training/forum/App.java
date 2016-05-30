aac2e36d340277a0c9efdfbab887b1e71471d56f
Antti Huokko <antti.huokko@solita.fi>
2016-05-30 14:29:35 +0300

    Added main class and main properties file

---
+++ src/main/java/fi/solita/training/forum/App.java
@@ -0,0 +1,11 @@
+package fi.solita.training.forum;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("hello world!!!"));
+	}
+
+}
